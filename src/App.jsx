f69ef3bb1e9033e05bbaa6cf243b57e6f7126dec
Enis Gayretli <enis@botanalytics.co>
2024-10-15 21:45:09 +0300

    UserContextProvider is added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import PageLayout from "./PageLayout";
-import Login from "./Login";
-import Signup from "./Signup";
+import PageLayout from "./components/PageLayout";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { UserContextProvider } from "./contexts/UserContext";
+
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
+      <UserContextProvider>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
 
-        <Route path="/" exact>
-          {/* /?variant=most_liked */}
-          <PageLayout>Home</PageLayout>
-        </Route>
-        <Route path="/profile/:nick">
-          <PageLayout>Profile page</PageLayout>
-        </Route>
-        <Route path="/detail/:twitId">
-          <PageLayout>Twit detail</PageLayout>
-        </Route>
-      </Switch>
+          <Route path="/" exact>
+            {/* /?variant=most_liked */}
+            <PageLayout>Home</PageLayout>
+          </Route>
+          <Route path="/profile/:nick">
+            <PageLayout>Profile page</PageLayout>
+          </Route>
+          <Route path="/detail/:twitId">
+            <PageLayout>Twit detail</PageLayout>
+          </Route>
+        </Switch>
+      </UserContextProvider>
     </div>
   );
 }
